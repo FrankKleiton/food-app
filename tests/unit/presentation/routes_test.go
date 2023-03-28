927b5b58f9319a899ec798f0b534381543674814
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-03-28 19:20:51 -0300

    Change api.

--- tests/unit/presentation/routes_test.go
+++ tests/unit/presentation/routes_test.go
@@ -1,6 +1,7 @@
 package presentation
 
 import (
+	"food-app/presentation/adapters"
 	httpServer "food-app/presentation/http"
 	"net/http"
 	"testing"
@@ -9,7 +10,7 @@ import (
 func TestRoutes(t *testing.T) {
 	t.Run("Add route", func(t *testing.T) {
 		routes := httpServer.Router{}
-		routes.AddRoute("GET", "/test", func(response httpServer.Response, request httpServer.Request) {
+		routes.AddRoute("GET", "/test", func(response adapters.IResponse, request adapters.IRequest) {
 		})
 		if len(routes.RoutesList) != 1 {
 			t.Errorf("Expected 1 route, got %d", len(routes.RoutesList))
@@ -19,13 +20,13 @@ func TestRoutes(t *testing.T) {
 	t.Run("Run route", func(t *testing.T) {
 		routes := httpServer.Router{}
 		ran := false
-		routes.AddRoute("GET", "/test", func(response httpServer.Response, request httpServer.Request) {
+		routes.AddRoute("GET", "/test", func(response adapters.IResponse, request adapters.IRequest) {
 			ran = true
 		})
 
 		request, _ := http.NewRequest("GET", "/test", nil)
 		response := http.ResponseWriter(nil)
-		routes.Route(httpServer.Response{HttpResponse: response}, httpServer.Request{HttpRequest: request})
+		routes.Route(httpServer.Response{HttpResponse: response}, &httpServer.Request{HttpRequest: request})
 
 		if ran != true {
 			t.Errorf("Expected true route, got %v", ran)
